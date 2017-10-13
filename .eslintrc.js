f10805f9660142a37e2f87d7170c6dcbef9cd37e
Thomas Kint <thomaskint.pro@gmail.com>
2017-10-13 02:01:02 +0200

    Added linebreak-style for windows in eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,6 +35,7 @@ module.exports = {
     }],
     // allow debugger during development
     'no-debugger': process.env.NODE_ENV === 'production' ? 2 : 0,
+    'linebreak-style': ['error', 'windows'],
     'no-param-reassign': ['error', { 'props': false }]
   }
 }
